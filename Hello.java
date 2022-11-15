d1b8c48b22bda8d14c319ee2fa75c2b47c1815ca
goondrex <andres2002dominguez@gmail.com>
2022-11-15 14:00:27 -0800

    feature-1 has added a different method

--- Hello.java
+++ Hello.java
@@ -4,4 +4,13 @@ public class Hello{
     String y = "World";
     System.out.println(x+y);
   }
+  public static int countConsonants(String text) {
+      return getConsonants(text).length();
+    }
+
+
+
+  public static boolean isConsonant(char c) {
+      return Character.isLetter(c) && !isVowel(c);
+    }
 }
